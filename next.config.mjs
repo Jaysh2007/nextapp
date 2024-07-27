5610deaf8dbc01511fc6c2a47e65f9452f9bf20b
Jaysh2007 <154827971+Jaysh2007@users.noreply.github.com>
2024-07-27 13:55:18 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output : 'export',
+    images :{
+        domains : ['images.unsplash.com','images.pexels.com','media.istockphoto.com']
+    }
+};
 
 export default nextConfig;
